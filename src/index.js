580132fbb5dde23f4d946e072e13c8d7f95c8c58
vmhungltt <vuminhhungltt904@gmail.com>
2022-09-07 17:02:48 +0700

    add serve for heroku

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 //const uploadFile = require ('express-fileupload') ;
 const cloudinary = require('cloudinary').v2;
-
+const http = require("http");
 const cors = require('cors');
 const router = require('./Routers/api');
 const express = require('express');
@@ -9,9 +9,10 @@ const morgan = require('morgan');
 const db = require('./Config/database/database');
 const app = express();
 const port = 5000;
+const server = http.createServer(app);
 db.connect();
 app.use(cors());
-//app.use(uploadFile());
+
 app.use(morgan('combined'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -23,14 +24,6 @@ cloudinary.config({
   api_secret: 'yr8KAay4lBEf9TfS9RVSdRQ0fk0',
   secure: true
 });
-
-///////////////////////
-
-
-
-
-////////////////////////////////
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-}) 
-
+server.listen(process.env.PORT || 5000, () => {
+  console.log(`server run  running at http://localhost:5000`);
+});
